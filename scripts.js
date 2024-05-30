637b5ad43f230a067a18dc59cf02cf6e45d8532b
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-30 01:46:56 +0100

    Enhanced error handling in calculate function with styled, temporary 'Invalid Command' message

--- scripts.js
+++ scripts.js
@@ -10,25 +10,17 @@ function clearDisplay() {
   display.value = "";
 }
 
-// function calculate() {
-//   try {
-//     display.value = eval(display.value);
-//   } catch (error) {
-//     display.value = "Error";
-//   }
-// }
-
 function calculate() {
   try {
     display.value = eval(display.value);
   } catch (error) {
     display.value = "*Invalid Command!";
-    display.style.color = "red"; // Set text color to red for error message
-    display.style.fontSize = "0.8rem"; // Set font size for the error message
+    display.style.color = "red";
+    display.style.fontSize = "0.8rem";
     setTimeout(() => {
-      display.value = ""; // Clear the error message after a short delay
-      display.style.color = "white"; // Reset text color to white;
-      display.style.fontSize = "3rem"; // Reset font size
-    }, 2000); // Clear the error message after 2 seconds (2000 milliseconds)
+      display.value = "";
+      display.style.color = "white";
+      display.style.fontSize = "3rem";
+    }, 2000); // Clear the error message after 2 seconds (2000 milliseconds); ✅
   }
 }
